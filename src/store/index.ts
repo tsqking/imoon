b697158e00b3c4d4baf8fe030f2fff49b684db35
charles <1214091885@qq.com>
2021-02-23 01:49:48 +0800

    integrate with redux-toolkit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from "redux";
+import { configureStore, MiddlewareArray } from "@reduxjs/toolkit";
+// import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import { rootReducer } from "./reducers";
 
 const logger = createLogger();
-const middleware = applyMiddleware(logger, thunk)
+// const middleware = applyMiddleware(logger, thunk)
+// export const store = createStore(rootReducer, middleware);
 
-export const store = createStore(rootReducer, middleware);
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: new MiddlewareArray().concat(thunk, logger)
+});
