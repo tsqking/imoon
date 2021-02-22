87fd867fe42dd38700e4338c598fffecb519c7f8
charles <1214091885@qq.com>
2021-02-23 02:02:08 +0800

    format code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,18 @@
-import { configureStore, MiddlewareArray } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { configureStore, MiddlewareArray } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 // import { applyMiddleware, createStore } from "redux";
-import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
-import { rootReducer } from "./reducers";
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
 
 const logger = createLogger();
 // const middleware = applyMiddleware(logger, thunk)
 // export const store = createStore(rootReducer, middleware);
 
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: new MiddlewareArray().concat(thunk, logger)
+  reducer: rootReducer,
+  middleware: new MiddlewareArray().concat(thunk, logger),
 });
 
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
